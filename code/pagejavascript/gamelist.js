d0b6c2562292d5f7d088bc241e14f1e1cc23a522
Jack Mathias <73205188+HumanoidsYoutube@users.noreply.github.com>
2021-01-12 12:11:04 -0600

    Yeet

--- code/pagejavascript/gamelist.js
+++ code/pagejavascript/gamelist.js
@@ -20,14 +20,46 @@ function start() {
         document.getElementById("g1").style.width = "650px";
         heading1.style.color = "white";
         heading1.style.fontSize = "45px";
-        document.getElementById("g1").innerHTML = "<br class = 'gamebreak'><img src = 'media/ratios.1' width = '500px' height='500px' class='gameimage'>";
+        var input = document.createElement("INPUT");
+        input.setAttribute("type", "text");
+        input.setAttribute("value", "Search...");
+        input.setAttribute("id", "search_input");
+        input.setAttribute("class", "search");
+        document.body.append(input)
+        var button = document.createElement("BUTTON");
+        button.setAttribute("onclick", "search()");
+        button.setAttribute("id", "searchbuttton");
+        button.setAttribute("class", "searchbutton")
+        button.innerHTML = "Go";
+        document.body.append(button)
     }, 3000)
-    setTimeout(function(){document.getElementById("g1").innerHTML = "<br class = 'gamebreak'><img src = 'media/ratios.1' width = '500px' height='500px' class='gameimage'><br class='gamebreak'><p class = 'gametitle'>Rates &#38; Ratios</p>"}, 3250)
-    setTimeout(function(){document.getElementById("g1").innerHTML = "<br class = 'gamebreak'><img src = 'media/ratios.1' width = '500px' height='500px' class='gameimage'><br class='gamebreak'><p class = 'gametitle'>Rates &#38; Ratios</p><br><p class = 'gamedec'>Rates &#38; Ratios is a game where you (MathMan) needs to solve rates and ratios problems, like Miles Per Hour and/or words per minute.</p>"}, 3500)
-    setTimeout(function(){document.getElementById("g1").innerHTML = `<br class = 'gamebreak'><img src = 'media/ratios.1' width = '500px' height='500px' class='gameimage'><br class='gamebreak'><p class = 'gametitle'>Rates &#38; Ratios</p><br><p class = 'gamedec'>Rates &#38; Ratios is a game where you (MathMan) needs to solve rates and ratios problems, like Miles Per Hour and/or words per minute.</p><br><a href = 'javascript:void(0)' title = 'Start the game :D' onclick='load("Ratios &#38; Rates", "games/ratesratios.html")'>Play now</a>`}, 4000)
 }
 //load the games
 function load(dec, game){
     var myWindow = window.open("", "");
-    myWindow.document.write(`<html><head><link rel = 'stylesheet' href = 'code/css/loadingpage.css' type = 'text/css'><link href = 'media/favicon-32x32.png' rel = 'icon' type = 'image/png'><title>Starting ` + dec + `...</title></head><body onload = 'loading()'><h1 id = 'head'>Preparing to launch ` + dec + `...</h1><br><p id = 'dis'><a>Click</a> <strong><a href = '` + game + `'>HERE</a></strong> <a>to</a> <a>play</a>.</p></body>`);
+    myWindow.document.write(`<html><head><link rel = 'stylesheet' href = 'code/css/loadingpage.css' type = 'text/css'><link href = 'media/favicon-32x32.png' rel = 'icon' type = 'image/png'><title>Starting ${dec}...</title></head><body onload = 'loading()'><h1 id = 'head'>Preparing to launch <u>${dec}</u></h1><br><p id = 'dis'><a>Click</a> <strong><a href = '${game}'>HERE</a></strong> <a>to</a> <a>play</a>.</p></body>`);
+}
+//  document.getElementById("myText").value = "Johnny Bravo";
+
+function search() {
+    var query = document.getElementById('search_input').value;
+    var query_lower = query.toLowerCase()
+    
+    if(query_lower.includes("rational")) {
+        var q = question('Are you looking for Rational Numbers?')
+        if(q = true) {
+            load('Rational Numbers', 'games/rationalnumbers.html')   
+        }
+    } else {
+        alert(`No results found for ${query}.`)
+    }
+    
+}
+
+function question(text) {
+    if (confirm(text)) {
+        return true;
+    } else {
+        return false;
+    }
 }
---
+++ code/pagejavascript/games/rational.js
@@ -0,0 +1,186 @@
+var i = 0;
+var txt = '';
+var speed = 50;
+var id = '';
+
+
+function start() {
+    var startbutton = document.getElementById('startbutton');
+    startbutton.remove()
+        var status = document.createElement('h1');
+        status.id = 'status'
+        status.innerHTML =''
+        document.body.appendChild(status)
+        type('Hello!', 'status', 50)
+        setTimeout(function() {
+            type('I see you there.', 'status', 50)
+            setTimeout(function() {
+                type('You can\'t hide from me.', 'status', 50)
+                setTimeout(function() {
+                    type('I will give you 10 seconds.', 'status', 50)
+                    setTimeout(function() {
+                        type('Try.', 'status', 200)
+                        document.body.style.backgroundColor = 'black';
+                        status.style.color = 'white';
+                        setTimeout(function() {
+                            status.style.color = 'black';
+                            var loc = prompt('Where do you want to hide?', 'basement')
+                                setTimeout(function(){
+                                    status.innerHTML = 'You failed. Great Job.'
+                                    alert(loc + ' did not work! Try somthing else next time...')
+                                    document.body.style.backgroundColor = 'white';
+                                        setTimeout(function(){
+                                            type('I will use this to take over the city!\nMathMan can\'t stop me now!', 'status', 50)
+                                            setTimeout(function(){
+                                                status.style.color = 'white';
+                                                setTimeout(function(){
+                                                    status.remove()
+                                                    part('1')
+                                                }, 1000)
+                                            }, 6000)
+                                        }, 4000)
+                                }, 6000)
+                        }, 4000)
+                    }, 3500)
+                }, 3500)
+            }, 3500)
+        }, 1500)
+    }
+
+function type(text, ii, sped) {
+    id = ii
+    txt = text
+    speed = sped
+    i = 0;
+    document.getElementById(id).innerHTML = '';
+    typeWriter()
+}
+
+function part(numberofpart) {
+    var title = document.getElementById('title');
+    var tip;
+    if(numberofpart === '1') {
+        title.innerHTML = 'Stage 1 | Rational Numbers | MathMan'
+        tip = document.createElement('p');
+        tip.innerHTML = 'Congrats! You completed Stage 1!'
+        tip.id = 'tip'
+        tip.style.background = 'none';
+        document.body.appendChild(tip)
+        setTimeout(function() {
+            tip.style.fontSize = 0;
+            
+        }, 1500)
+        var clipart = document.createElement('img');
+        clipart.src = 'https://humanoidsyoutube.github.io/MathManAbout/media/images/favicon/favicon%201.svg';
+        clipart.id = 'mathman'
+        document.body.appendChild(clipart)
+        var speech = document.createElement('h1');
+        speech.innerHTML = '';
+        speech.id = 'mmtalk';
+        document.body.appendChild(speech);
+        type('It is almost time to go.', 'mmtalk', 55)
+        setTimeout(function() {
+            type('Why?', 'mmtalk', 100)
+            clipart.src = '../../../../media/sidekick.svg'
+            setTimeout(function() {
+                type('They are coming.', 'mmtalk', 250)
+                clipart.src = 'https://humanoidsyoutube.github.io/MathManAbout/media/images/favicon/favicon%201.svg'
+                setTimeout(function() {
+                    clipart.style.opacity = 0;
+                    speech.style.opacity = 0;
+                    document.body.style.backgroundColor = 'black'
+                    setTimeout(function() {
+                        part('2')
+                    }, 1000)
+                }, 5000)
+            }, 1500)
+        }, 2000)
+    } else if(numberofpart === '2') {
+        tip = document.getElementById('tip');
+        document.body.style.backgroundColor = '#ff5608';
+        tip.style.fontSize = 20;
+        tip.innerHTML = 'New battle!';
+        setTimeout(function(){
+            tip.style.fontSize = 0;
+        }, 1000)
+    }
+}
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById(id).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+    function setCookie(name, value, ex) {
+  var d = new Date();
+  d.setTime(d.getTime() + (ex * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function grab(query) {
+  var name = query + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function check(ccookie, question, defaultstring, minlength, maxlength, r) {
+  var cookie = grab(ccookie);
+  var res;
+  if (cookie != "") {
+      return cookie;
+  } else {
+    if(question === '-none-') {
+        res = defaultstring;
+    } else {
+        res = ask(question, minlength, maxlength, defaultstring);
+    }
+    if (res != "" && res != null) {
+      setCookie(ccookie, res, 365);
+      return res;
+    }
+  }
+}
+function ask(ask, minl, maxl, def, reload) {
+    var responce = prompt(ask, def);
+    if(responce == null || responce == "") {
+        alert("Please provide a responce.");
+        ask(ask, minl, maxl);
+        return "Error - Failed to provide a correct responce.";
+    } else if (responce.length <= minl) {
+        alert("You need to provide a responce longer than " + minl + " characters.\n\nYour responce:   " + responce);
+        ask(ask, minl, maxl);
+        return "Error - Failed to provide a correct responce.";
+    } else if (responce.length >= maxl) {
+        alert("You need to provide a responce shorter than " + maxl + " characters.");
+        ask(ask, minl, maxl);
+        return "Error - Failed to provide a correct responce.";
+    } else {
+        return responce;
+    }
+}
+
+function clearloader() {
+    setTimeout(function(){
+        document.getElementById('loader').style.height = 0;
+        document.getElementById('loadertop').style.height = 0;
+        document.getElementById('loaderbottom').style.height = 0;
+        setTimeout(function(){
+            document.getElementById('loader').remove()
+            document.getElementById('loadertop').remove()
+            document.getElementById('loaderbottom').remove()
+        }, 1000)
+    }, 2500)
+}
